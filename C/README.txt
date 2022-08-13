4fe558bd5b39f1150538394b7da810b294216984
Felipe Toledo <toledofelipe98@gmail.com>
2022-08-13 11:37:32 -0300

    transfering files from the other repository

---
+++ C/README.txt
@@ -0,0 +1,1 @@
+Todos os projetos são feitos utilizando a IDE codeblocks, a premissa de cada projeto está no escopo do código final
---
+++ day_1_brand_generator.py
@@ -0,0 +1,12 @@
+# Create a brand generator which will concatenate the name of the city and your's pet name using the input method. 
+# Steps: 
+# 1.Create a greeting for your program
+print('#######################################')
+print('Welcome to the brand generator program!')
+print('#######################################\n')
+# 2.Ask the user to input the city that he/she grew up
+city = input("Which city did you grow up ? \n")
+# 3.Ask the user the name of a pet
+pet = input('Whats is the name of a pet ?\n')
+# 4.Combine the name of their city and pet and show them their brand name
+print("Your brand name could be: "+city+ " " +pet+ " or: " +pet+ " " + city)
